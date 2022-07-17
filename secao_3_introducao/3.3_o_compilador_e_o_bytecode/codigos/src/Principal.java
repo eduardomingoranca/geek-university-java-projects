027a7dcbb2c4dd19ee848ac34630dd6063469290
eduardomingoranca <eduardomingoranca@gmail.com>
2022-07-17 14:29:27 -0300

    add bytecode and compile

---
+++ secao_3_introducao/3.3_o_compilador_e_o_bytecode/codigos/src/Principal.java
@@ -0,0 +1,13 @@
+// O compilador e o bytecode
+public class Principal {
+
+    public static void main(String[] args) {
+        // Declarando e inicializando variavel
+        int idade = 28;
+
+        System.out.println("Eu tenho " + idade + " anos.");
+
+        // javac Programa.java -> Programa.class -> java Programa
+        // codigo fonte (Programa.java) -> bytecode (Programa.class) -> executavel
+    }
+}
