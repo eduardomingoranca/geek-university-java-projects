cdb22122f79a7026055f66a205aacbde56de24d2
eduardomingoranca <eduardomingoranca@gmail.com>
2022-07-17 14:21:10 -0300

    add variables

---
+++ secao_3_introducao/3.2_variaveis/codigos/src/Programa01.java
@@ -0,0 +1,30 @@
+// Variaveis
+public class Programa01 {
+
+    public static void main(String[] args) {
+        // Declaracao de variavel
+        int valor;
+
+        // Declaracao de variavel
+        int num1, num2, num3;
+
+        // Declarando e inicializando algumas variaveis
+        int num4, num5 = 4, num6 = 7, num8;
+
+        char caractere;
+        float preco;
+
+        // Declarando
+        int num9;
+
+        // Declarando e inicalizando
+        int num10 = 99;
+
+        // int 10eu = 7; // Errado
+
+        float _valor = 34.2f; // Por padrao, os valores de ponto flutuante sao double no java
+
+        double _valor2 = 34.2;
+
+    }
+}
