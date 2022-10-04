0f56950ddc254b93573d35180568742105e72d3a
eduardomingoranca <eduardomingoranca@gmail.com>
2022-10-04 15:58:28 -0300

    add challenge

---
+++ secao_8_funcoes/desafios/codigos/src/Desafio61.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+  Escreva uma funcao que compare e retorne verdadeiro, caso uma string seja anagrama
+  da outra, e falso, caso contrario.
+ */
+public class Desafio61 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
+    }
+
+}
