6d78a030c01b8551abdd18a5ee8e455f1ab9fc8f
eduardomingoranca <eduardomingoranca@gmail.com>
2022-10-15 10:31:42 -0300

    add classes

---
+++ secao_9_poo/9.2_classes/codigos/src/Produto.java
@@ -0,0 +1,14 @@
+/**
+ * Classes
+ * O nome das classes inicia com letra maiuscula;
+ * O nome nao deve conter: Acentuacao, caracteres especiais, espaco
+ * Nas classes Java, nao existe a implementacao da funcao main()
+ * Toda classe Java possui a seguinte forma:
+ * O nome da classe Java DEVE ser o mesmo nome do arquivo java.
+ * Tudo que estiver dentro das "chaves" {} faz parte da classe.
+ * <p>
+ * public class NomeDaClasse { }
+ */
+public class Produto {
+
+}
