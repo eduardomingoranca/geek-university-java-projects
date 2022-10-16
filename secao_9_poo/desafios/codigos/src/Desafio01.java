ebc0539998f044cd1e5b6ca0216555241f83be2e
eduardomingoranca <eduardomingoranca@gmail.com>
2022-10-16 11:18:06 -0300

    add challenge

---
+++ secao_9_poo/desafios/codigos/src/Desafio01.java
@@ -0,0 +1,10 @@
+public class Desafio01 {
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Ernst Hensler");
+        pessoa.setIdade(63);
+        pessoa.setAltura(1.90);
+        pessoa.imprimeDados();
+    }
+}
