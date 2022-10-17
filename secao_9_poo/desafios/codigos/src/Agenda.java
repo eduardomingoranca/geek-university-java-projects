a9158c488d2f8b624666c6b6182346206f757695
eduardomingoranca <eduardomingoranca@gmail.com>
2022-10-17 08:19:51 -0300

    add challenge

---
+++ secao_9_poo/desafios/codigos/src/Agenda.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Vector;
+
+public class Agenda {
+    private Pessoa pessoa;
+    private final List<Agenda> agenda = new ArrayList<>();
+    private final Vector<String> names = new Vector<>();
+
+    public Agenda() { }
+
+    public Agenda(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public List<Agenda> getAgenda() {
+        return agenda;
+    }
+
+    public void armazenaPessoa(String nome, int idade, double altura) {
+        pessoa = new Pessoa();
+        pessoa.setNome(nome);
+        pessoa.setIdade(idade);
+        pessoa.setAltura(altura);
+
+        Agenda contato = new Agenda(pessoa);
+
+        System.out.println(contato.getPessoa().getNome() + "; " + contato.getPessoa().getIdade() + "; "
+                + contato.getPessoa().getAltura());
+        if (agenda.size() < 2) {
+            agenda.add(contato);
+            names.add(contato.getPessoa().getNome());
+            System.out.println(agenda.size() + " Contato Cadastrado.");
+        } else if (agenda.size() <= 9){
+            agenda.add(contato);
+            names.add(contato.getPessoa().getNome());
+            System.out.println(agenda.size() + " Contatos Cadastrados.");
+        } else System.out.println("Capacidade máxima de " +  agenda.size() + " atiginda.");
+    }
+
+    public void removePessoa(String nome) {
+        for (int j = 0; j < agenda.size(); j++) {
+            if (Objects.equals(nome, agenda.get(j).getPessoa().getNome())){
+                agenda.remove(j);
+                names.remove(j);
+            System.out.println("\nO contato foi excluido com sucesso.");
+            } else System.out.println("\nO contato não está registrado na agenda!");
+        }
+    }
+
+    public int buscaPessoa(String nome) {
+        int verify = names.indexOf(nome);
+        int position = 0;
+
+        for (int i = 0; i < agenda.size(); i++) {
+            if (verify != -1) {
+                position = verify + 1;
+                break;
+            }
+        }
+
+        return position;
+    }
+
+
+    @Override
+    public String toString() {
+        return pessoa.getNome() + "; " + pessoa.getIdade() + "; " + pessoa.getAltura();
+    }
+
+    public void imprimirAgenda() {
+        System.out.println("");
+        for (Agenda set : agenda)
+            System.out.println(set);
+    }
+
+    public void imprimirPessoa(int i) {
+        System.out.println("\nA agenda atualmente possui " + agenda.size() + " contatos.");
+        System.out.println("\nO contato " + names.get(i) + " cadastrado na posição " + (i+1) + ".");
+        System.out.println("\nPossui os seguintes dados: " + agenda.get(i));
+    }
+
+}
---
+++ secao_9_poo/desafios/codigos/src/Desafio02.java
@@ -0,0 +1,146 @@
+import java.util.Scanner;
+
+public class Desafio02 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Agenda contato = new Agenda();
+        String acao = "";
+        String continuar;
+
+        System.out.println("Bem vindo a Agenda!\n");
+        do {
+            System.out.println("==================================");
+            System.out.println("| 1 |  ARMAZENAR um novo contato.");
+            System.out.println("| 2 |  REMOVER um contato.");
+            System.out.println("| 3 |  BUSCAR um novo contato.");
+            System.out.println("| 4 |  IMPRIMIR TODOS os contatos.");
+            System.out.println("| 5 |  IMPRIMIR um único contato.");
+            System.out.println("| 0 |  SAIR da agenda.");
+            System.out.println("==================================");
+
+            System.out.print("Selecione uma opcao que deseja acessar: ");
+            int opcao = Integer.parseInt(scanner.nextLine());
+
+            switch (opcao) {
+                case 1:
+
+                    do {
+                        System.out.print("Informe o nome do contato: ");
+                        String nome = scanner.nextLine();
+
+                        System.out.print("Informe a idade: ");
+                        int idade = Integer.parseInt(scanner.nextLine());
+
+                        System.out.print("Informe a altura: ");
+                        double altura = Double.parseDouble(scanner.nextLine());
+
+                        contato.armazenaPessoa(nome, idade, altura);
+                        if(contato.getAgenda().size() <= 9) {
+                            System.out.print("\nDeseja adicionar outro contato? [S/N]");
+                            continuar = scanner.nextLine();
+                        }else {
+                            continuar = "n";
+                            System.out.print("\nCapacidade máxima de " +  contato.getAgenda().size() + " atiginda.");
+                        }
+
+                        if (continuar.equalsIgnoreCase("n")) {
+                            System.out.print("\nDeseja realizar uma nova ação? [S/N]");
+                            acao = scanner.nextLine();
+                            if (acao.equalsIgnoreCase("n")) {
+                                System.out.print("\nA agenda está sendo encerrada, até a próxima!");
+                                System.exit(0);
+                            }
+                        }
+                    } while (continuar.equalsIgnoreCase("s"));
+                    break;
+
+                case 2:
+                    do {
+                        System.out.print("Informe o nome do contato: ");
+                        String nome = scanner.nextLine();
+
+                        contato.removePessoa(nome);
+
+                        System.out.print("\nDeseja remover outro contato? [S/N]");
+                        continuar = scanner.nextLine();
+
+                        if (continuar.equalsIgnoreCase("n")) {
+                            System.out.print("\nDeseja realizar uma nova ação? [S/N]");
+                            acao = scanner.nextLine();
+                            if (acao.equalsIgnoreCase("n")) {
+                                System.out.print("\nA agenda está sendo encerrada!");
+                                System.exit(0);
+                            }
+                        }
+                    } while (continuar.equalsIgnoreCase("s"));
+                    break;
+
+                case 3:
+                    do {
+                        System.out.print("Informe o nome do contato: ");
+                        String nome = scanner.nextLine();
+
+                        if (contato.buscaPessoa(nome) == 1)
+                            System.out.println(contato.getPessoa().getNome() + "; " + contato.getPessoa().getIdade() + "; "
+                                    + contato.getPessoa().getAltura());
+                        else System.out.println("\nO contato não consta na agenda!");
+
+                        System.out.print("\nDeseja buscar outro contato? [S/N]");
+                        continuar = scanner.nextLine();
+
+                        if (continuar.equalsIgnoreCase("n")) {
+                            System.out.print("\nDeseja realizar uma nova ação? [S/N]");
+                            acao = scanner.nextLine();
+                            if (acao.equalsIgnoreCase("n")) {
+                                System.out.print("\nA agenda está sendo encerrada!");
+                                System.exit(0);
+                            }
+                        }
+
+                    } while (continuar.equalsIgnoreCase("s"));
+                    break;
+
+                case 4:
+                    contato.imprimirAgenda();
+                    System.out.print("\nDeseja realizar uma nova ação? [S/N]");
+                    acao = scanner.nextLine();
+                    if (acao.equalsIgnoreCase("n")) {
+                        System.out.print("\nA agenda está sendo encerrada!");
+                        System.exit(0);
+                    }
+                    break;
+
+                case 5:
+                    do {
+                        System.out.print("Informe o numero do contato que deseja imprimir: ");
+                        int n = Integer.parseInt(scanner.nextLine());
+
+                        contato.imprimirPessoa(n);
+
+                        System.out.print("\nDeseja buscar outro contato? [S/N]");
+                        continuar = scanner.nextLine();
+
+                        if (continuar.equalsIgnoreCase("n")) {
+                            System.out.print("\nDeseja realizar uma nova ação? [S/N]");
+                            acao = scanner.nextLine();
+                            if (acao.equalsIgnoreCase("n")) {
+                                System.out.print("\nA agenda está sendo encerrada!");
+                                System.exit(0);
+                            }
+                        }
+
+                    } while (continuar.equalsIgnoreCase("s"));
+                    break;
+                case 0:
+                    acao = "n";
+                    System.out.print("\nA agenda está sendo encerrada, até a próxima!");
+                    System.exit(0);
+                    break;
+            }
+        } while (acao.equalsIgnoreCase("s"));
+
+        scanner.close();
+    }
+}
