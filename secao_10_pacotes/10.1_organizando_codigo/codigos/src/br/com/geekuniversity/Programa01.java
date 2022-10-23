46002054995fe74ffa2ab02a0ac77c0394e3b26f
eduardomingoranca <eduardomingoranca@gmail.com>
2022-10-23 09:46:44 -0300

    add packages

---
+++ secao_10_pacotes/10.1_organizando_codigo/codigos/src/br/com/geekuniversity/Programa01.java
@@ -0,0 +1,22 @@
+package br.com.geekuniversity;
+
+/**
+ * Pacotes
+ * - Pacotes servem para organizar o codigo fonte do projeto;
+ * - Pacotes indicam onde determinado arquivo do projeto se encontra;
+ * - Os nomes de pacotes devem ser em letras minusculas, sem espaco, sem
+ * caractere especial, sem acentuacao;
+ * - Por convencao, eh utilizado nos nomes de pacotes, o dominio da empresa
+ * criadora do projeto, ao contrario, sendo a ultima parte do nome
+ * o especificador do pacote.
+ * <p>
+ * - Padrao de Projeto
+ * - MVC [Model View Controller]
+ */
+
+public class Programa01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
