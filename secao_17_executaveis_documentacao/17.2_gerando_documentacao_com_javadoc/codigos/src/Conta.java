48ae5eadf02d5f5a6afad77bad4a401d181f3010
eduardomingoranca <eduardomingoranca@gmail.com>
2022-11-12 15:43:16 -0300

    add javadoc files

---
+++ secao_17_executaveis_documentacao/17.2_gerando_documentacao_com_javadoc/codigos/src/Conta.java
@@ -0,0 +1,58 @@
+public class Conta {
+    private final int numero;
+    private Double saldo;
+    private Double limite;
+    private final Cliente cliente;
+
+    public Conta(int numero, Double saldo, Double limite, Cliente cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.limite = limite;
+        this.cliente = cliente;
+    }
+
+    public void sacar(double valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+            System.out.println("Saque realizado com sucesso!");
+        } else if (valor <= (this.saldo + this.limite)) {
+            double value = this.saldo - valor;
+            if (value < 0.0)
+                this.saldo = 0.0;
+
+            value = this.limite + value;
+            this.limite = value;
+            System.out.println("Saque realizado com sucesso!");
+        } else System.out.println("Saldo insuficiente!");
+    }
+
+    /**
+     * Metodo para realizar deposito
+     *
+     * @param valor a ser depositado
+     */
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    /**
+     * Metodo getter do atributo Saldo
+     *
+     * @return a soma do saldo + limite
+     */
+    public double getSaldo() {
+        return this.saldo + this.limite;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Double getLimite() {
+        return limite;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+}
