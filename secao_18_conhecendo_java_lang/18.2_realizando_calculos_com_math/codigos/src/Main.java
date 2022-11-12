c1bb1b25c2bc8837a4564f7e3077a92a3bbbcc10
eduardomingoranca <eduardomingoranca@gmail.com>
2022-11-12 16:54:00 -0300

    add math class

---
+++ secao_18_conhecendo_java_lang/18.2_realizando_calculos_com_math/codigos/src/Main.java
@@ -0,0 +1,40 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+import static java.lang.Math.*;
+
+/**
+ * Realizando calculos com Math
+ * <p>
+ * A biblioteca Math possui varios metodos e constantes estaticas
+ * para que possamos utilizar nos nossos programas.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(E); // Constante
+
+        System.out.println(PI);
+
+        System.out.println(sin(45));
+
+        System.out.println(cos(45));
+
+        System.out.println(tan(45));
+
+        System.out.println(round(PI));
+
+        System.out.println(ceil(PI));
+
+        System.out.println(floor(PI));
+
+        System.out.println(pow(3, 2));
+
+        System.out.println(round(pow(3, 2)));
+
+        System.out.println(floor(pow(3, 2)));
+
+        System.out.println(sqrt(9));
+
+        System.out.println(round(sqrt(9)));
+    }
+}
